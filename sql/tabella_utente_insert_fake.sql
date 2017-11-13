af73a4569fb0fc69daa206c38b4108f46c7de199
archistico <info@archistico.com>
2017-11-13 18:26:11 +0100

    sql nuovo cliente

--- sql/tabella_utente_insert_fake.sql
+++ sql/tabella_utente_insert_fake.sql
@@ -1,4 +1,5 @@
 INSERT INTO utente VALUES (null, "Rossi Mario", "Via Faraone, 1 - 12345 Piramide", "codicefiscale", "partitaiva", "numerotelefono", "indirizzo@email.com", "toor", 1, "-");
 INSERT INTO utente VALUES (null, "Verdi Pasquale", "Via Faraone, 2 - 12345 Piramide", "codicefiscale", "partitaiva", "numerotelefono", "indirizzo@email.com", "toor", 2, "-");
 INSERT INTO utente VALUES (null, "Giallo Viola", "Via Faraone, 3 - 12345 Piramide", "codicefiscale", "partitaiva", "numerotelefono", "indirizzo@email.com", "toor", 3, "-");
+INSERT INTO utente VALUES (null, "Micio Miao", "Via Faraone, 3 - 12345 Piramide", "codicefiscale", "partitaiva", "numerotelefono", "indirizzo@email.com", "toor", 3, "-");
 
