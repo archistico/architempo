fc4e6b327347f9b2bad48bfb612d48a525c9ab3d
archistico <info@archistico.com>
2017-11-13 18:54:04 +0100

    add template e generale progetto elimina

---
+++ progetto_elimina.php
@@ -0,0 +1,69 @@
+<?php
+require_once('classi/file.php');
+$filename_corrente = File::FILENAME(__FILE__);
+$basename_corrente = File::BASENAME(__FILE__);
+require_once('loader.php');
+
+Html_default::HEAD("Architempo - ".strtoupper($filename_corrente));
+Html_default::OPENCONTAINER();
+Html_default::MENU($basename_corrente);
+Html_default::JUMBOTRON("Studio Archistico", "Time tracker");
+
+/* -----------------------------
+ *           LOGGIN
+ * -----------------------------
+ */
+$utentefk = Utente::UTENTE_LOGGATO_ID();
+
+/* -----------------------------
+ *       CORPO FILE
+ * -----------------------------
+ */
+
+// VALIDAZIONE
+if (empty($_GET['id'])) {
+    $notices[] = 'ID non inserito';
+} else {
+    $id = Utilita::PULISCISTRINGA($_GET['id']);
+}
+if (!isset($_GET['ok'])) {
+    $notices[] = 'Bypass non attivo';
+} else {
+    $ok = Utilita::PULISCISTRINGA($_GET['ok']);
+}
+
+// CONTROLLO ID ESISTENTE
+if (empty($notices) && !Progetto::EXIST($id)) {
+    $notices[] = 'Nessun progetto con questo ID';
+}
+
+// MOSTRO LA SCELTA
+if(empty($notices) && $ok != 1) {
+    $progetto = new Progetto();
+    $progetto->getDataByID($id);
+    $HTML->scelta("ATTENZIONE! CANCELLARE IL PROGETTO E I RELATIVI TEMPI?", $progetto->getInfo(), "CANCELLA", "$basename_corrente?id=$id&ok=1", "progetto.php");
+}
+
+// SE INVECE HO ACCETTATO
+if(empty($notices) && $ok == 1) {
+    $progetto = new Progetto();
+    $progetto->getDataByID($id);
+
+    // CANCELLA DAL DB
+    if(!Progetto::DELETE_BY_ID_AND_TEMPO($id)) {
+        $notices[] = 'Errore nella cancellazione sulla base dati';
+    }
+    $notices['ok'] = "Tempo cancellato";
+}
+
+Html_default::SHOW_NOTICES($notices, "progetto.php");
+
+/* -----------------------------
+*      FINE CORPO FILE
+* -----------------------------
+*/
+
+// Elementi di chiusura
+Html_default::CLOSECONTAINER();
+Html_default::SCRIPT(True);
+Html_default::END();
