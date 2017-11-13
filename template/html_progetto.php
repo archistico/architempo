e027a3ce63424a6d447a280307fd23661b3efc98
archistico <info@archistico.com>
2017-11-13 14:38:37 +0100

    d-none d-md-table-cell funzionante

--- template/html_progetto.php
+++ template/html_progetto.php
@@ -3,19 +3,20 @@
 class Html_progetto {
 
     public function Table_progetto($progetti) {
-        $columShowOnlyLarge = 'd-none d-md-block';
+        $colonneVisibili = '';
+        $colunneNascoste = 'd-none d-md-table-cell';
         $html = "
         <div class='row'>
         <div class='col-md-12'>
         <table class='table table-bordered'>
           <thead>
             <tr>
-              <th>Descrizione</th>
-              <th class='$columShowOnlyLarge'>Tipologia</th>
-              <th class='$columShowOnlyLarge'>Acconto/Compenso</th>
-              <th class='$columShowOnlyLarge'>Pagato/Completato</th>
-              <th>Tempo</th>
-              <th>#</th>
+              <th class='$colonneVisibili'>Descrizione</th>
+              <th class='$colunneNascoste'>Tipologia</th>
+              <th class='$colunneNascoste'>Acconto/Compenso</th>
+              <th class='$colunneNascoste'>Pagato/Completato</th>
+              <th class='$colonneVisibili'>Tempo</th>
+              <th class='$colonneVisibili'>#</th>
             </tr>
           </thead>
           <tbody>
@@ -23,12 +24,12 @@ class Html_progetto {
         echo $html;
         foreach ($progetti as $elemento) {
             echo "<tr>";
-            echo " <td>".$elemento->descrizione." - ".$elemento->getCliente()->denominazione."</td>";
-            echo " <td class='$columShowOnlyLarge'>".$elemento->getTipologia()->descrizione."</td>";
-            echo " <td class='$columShowOnlyLarge'>&euro; ".$elemento->acconto." / &euro; ".$elemento->compenso."</td>";
-            echo " <td class='$columShowOnlyLarge'>".($elemento->pagato==0?'NO':'SÍ')." - ".($elemento->completato==0?'NO':'SÍ')."</td>";
-            echo " <td>".$elemento->getTempo()."</td>";
-            echo " <td><a href='progetto_elimina.php?id=$elemento->progettoid&ok=0'><i class='fa fa-times fa-lg rosso' aria-hidden='true'></i></a></td>";
+            echo " <td class='$colonneVisibili'>".$elemento->descrizione." - ".$elemento->getCliente()->denominazione."</td>";
+            echo " <td class='$colunneNascoste'>".$elemento->getTipologia()->descrizione."</td>";
+            echo " <td class='$colunneNascoste'>&euro; ".$elemento->acconto." / &euro; ".$elemento->compenso."</td>";
+            echo " <td class='$colunneNascoste'>".($elemento->pagato==0?'NO':'SÍ')." - ".($elemento->completato==0?'NO':'SÍ')."</td>";
+            echo " <td class='$colonneVisibili'>".$elemento->getTempo()."</td>";
+            echo " <td class='$colonneVisibili'><a href='progetto_elimina.php?id=$elemento->progettoid&ok=0'><i class='fa fa-times fa-lg rosso' aria-hidden='true'></i></a></td>";
             echo "</tr>";
         }
         $html = "
--- template/html_tempo.php
+++ template/html_tempo.php
@@ -3,6 +3,7 @@
 class Html_tempo {
 
     public function Table_tempo($tempi) {
+        $colunneNascoste = 'd-none d-md-table-cell';
         $html = "
         <div class='row'>
         <div class='col-md-12'>
@@ -11,8 +12,8 @@ class Html_tempo {
             <tr>
               <th>Progetto</th>
               <th>Descrizione</th>
-              <th class='hidden-xs'>Inizio</th>
-              <th class='hidden-xs'>Fine</th>
+              <th class='$colunneNascoste'>Inizio</th>
+              <th class='$colunneNascoste'>Fine</th>
               <th>Durata</th>
               <th>#</th>
             </tr>
@@ -24,8 +25,8 @@ class Html_tempo {
             echo "<tr>";
             echo " <td>".$tem->getProgetto()->descrizione."</td>";
             echo " <td>".$tem->descrizione."</td>";
-            echo " <td class='hidden-xs'>".$tem->getDatainizio()."</td>";
-            echo " <td class='hidden-xs'>".$tem->getDatafine()."</td>";
+            echo " <td class='$colunneNascoste'>".$tem->getDatainizio()."</td>";
+            echo " <td class='$colunneNascoste'>".$tem->getDatafine()."</td>";
             echo " <td>".$tem->getDurata()."</td>";
             echo " <td><a href='tempo_elimina.php?id=$tem->tempoid&ok=0'><i class='fa fa-times fa-lg rosso' aria-hidden='true'></i></a></td>";
             echo "</tr>";
