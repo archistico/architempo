6817f8f539120809e409afdd9e4af12e4e3bb404
archistico <info@archistico.com>
2017-11-15 00:58:25 +0100

    api statistiche tipologie ok

--- api_tipologie.php
+++ api_tipologie.php
@@ -5,31 +5,23 @@ header('Content-Type: application/json');
 require_once('config.php');
 require_once('classi/utilita.php');
 Utilita::PARAMETRI_INIZIALI();
-
-require_once('classi/ruolo.php');
-require_once('classi/utente.php');
 require_once('classi/database.php');
-require_once('classi/tipologia.php');
-require_once('classi/progetto.php');
-require_once('classi/tempo.php');
 
 $data = array();
+$oggetti = array();
 
-function somma () {
-
+function differenza_ore($inizio, $fine) {
     $totale = 0;
-    foreach ($this->tempi as $t) {
-        $secondi = $t->getDatainizioObj()->diff($t->getDatafineObj())->format("%S");
-        $minuti = $t->getDatainizioObj()->diff($t->getDatafineObj())->format("%I");
-        $ore = $t->getDatainizioObj()->diff($t->getDatafineObj())->format("%H");
-        $giorni = $t->getDatainizioObj()->diff($t->getDatafineObj())->format("%D");
 
-        $totale += $ore + $giorni * 24 + $minuti /60 + $secondi/3600;
-    }
-    return number_format($totale, 1, ',', ' ');
+    $secondi = $inizio->diff($fine)->format("%S");
+    $minuti = $inizio->diff($fine)->format("%I");
+    $ore = $inizio->diff($fine)->format("%H");
+    $giorni = $inizio->diff($fine)->format("%D");
 
+    $totale += $ore + $giorni * 24 + $minuti /60 + $secondi/3600;
+
+    return $totale;
 }
-
 
 try {
     $database = new db();
@@ -43,13 +35,32 @@ try {
 
     foreach ($rows as $row) {
         $tipologia = $row['descrizione'];
+        $inizio = DateTime::createFromFormat('Y-m-d H:i:s', $row['datainizio']);
+        $fine = DateTime::createFromFormat('Y-m-d H:i:s', $row['datafine']);
 
-        $t = new Tempo();
+        $riga = [];
+        $riga['tipologia'] = $tipologia;
+        $riga['ore'] = differenza_ore($inizio, $fine);
 
-        $t->setDatainizioDB($row['datainizio']);
-        $t->setDatafineDB($row['datafine']);
-        $data[] = $row;
+        $tempi[] = $riga;
     }
+
+    foreach ($tempi as $riga) {
+        if(in_array($riga['tipologia'], array_column($oggetti, 'tipologia'))) {
+            $key = array_search($riga['tipologia'], array_column($oggetti, 'tipologia'));
+            $oggetti[$key]['ore'] += $riga['ore'];
+        } else {
+            $oggetti[] = $riga;
+        }
+    }
+
+    foreach ($oggetti as $ris) {
+        $riga = [];
+        $riga[] = $ris['tipologia'];
+        $riga[] = $ris['ore'];
+        $data[] = $riga;
+    }
+
 } catch (PDOException $e) {
     throw new PDOException("Error  : " . $e->getMessage());
 }
