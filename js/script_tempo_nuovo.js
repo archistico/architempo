74fcac3901c44650d7ebeb8c22fdc0c1ab632544
archistico <info @archistico.com>
2017-11-16 10:30:54 +0100

    datepicker autoclose, lunedi per primo

--- js/script_tempo_nuovo.js
+++ js/script_tempo_nuovo.js
@@ -1,9 +1,15 @@
 $(function () {
     $('#datainizio').datepicker({
         format: 'dd/mm/yyyy',
+        todayHighlight: true,
+        weekStart: 1,
+        autoclose: true
     });
 
     $('#datafine').datepicker({
         format: 'dd/mm/yyyy',
+        todayHighlight: true,
+        weekStart: 1,
+        autoclose: true
     });
 });
