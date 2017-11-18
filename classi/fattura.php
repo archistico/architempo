f7771a51d7ebfe32bc7f9e13cfbcf233f73da2b4
archistico <info@archistico.com>
2017-11-18 15:55:07 +0100

    sql fattura

--- classi/fattura.php
+++ classi/fattura.php
@@ -13,6 +13,10 @@ class Fattura
     public $progetto;
     public $tipologiafattura;
 
+    public function getInfo() {
+        return "";
+    }
+
     public function getProgetto() {
         return $this->progetto;
     }
@@ -21,6 +25,34 @@ class Fattura
         return $this->tipologiafattura;
     }
 
+    // SETTA DATA
+    public function setData($data) {
+        $this->data = DateTime::createFromFormat('d/m/Y H:i:s', $data);
+    }
+
+    // RESTITUISCI DATA STRINGA
+    public function getDataStr() {
+        return $this->data->format('d/m/Y H:i:s');
+    }
+
+    // RESTITUISCI DATA STRINGA
+    public function getAnno() {
+        return $this->data->format('Y');
+    }
+
+    // RESTITUISCI DATA OGGETTO
+    public function getDataObj() {
+        return $this->data;
+    }
+
+    // DB
+    public function getDataDB() {
+        return $this->data->format('Y-m-d H:i:s');
+    }
+
+    public function setDataDB($data) {
+        $this->data = DateTime::createFromFormat('Y-m-d H:i:s', $data);
+    }
 
 }
 
---
+++ sql/tabella_fattura.sql
@@ -0,0 +1,17 @@
+CREATE TABLE `fattura` (
+  `fatturaid` int(11) NOT NULL,
+  `numero` int(11) NOT NULL,
+  `anno` int(11) NOT NULL,
+  `data` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  `progettofk` int(11) NOT NULL,
+  `oggetto` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `importo` DECIMAL(10,2) NOT NULL DEFAULT 0,
+  `totale` DECIMAL(10,2) NOT NULL DEFAULT 0,
+  `tipologiafatturafk` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+ALTER TABLE `fattura`
+  ADD PRIMARY KEY (`fatturaid`);
+
+ALTER TABLE `fattura`
+  MODIFY `fatturaid` int(11) NOT NULL AUTO_INCREMENT;
